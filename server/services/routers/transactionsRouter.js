330630ef6218473f8ca089abb986992dfd89f6df
Itay Tur <unlessmusic1@gmail.com>
2021-04-04 23:44:23 +0300

    server logs

--- server/services/routers/transactionsRouter.js
+++ server/services/routers/transactionsRouter.js
@@ -5,7 +5,9 @@ const transactionsRouter = new express.Router();
 
 transactionsRouter.get('/transactions', (req, res) => {
     try {
+        console.log('[transactionsRouter.js] get req:', req);
         const transactions = transactionsManager.getTransactionsByUserId('itay');
+        console.log('[transactionsRouter.js] get res:', transactions);
         res.send(transactions);
     } catch (error) {
         res.status(500).send({ error })
@@ -14,7 +16,9 @@ transactionsRouter.get('/transactions', (req, res) => {
 
 transactionsRouter.post('/transactions', (req, res) => {
     try {
+        console.log('[transactionsRouter.js] post req:', req);
         const newTransaction = transactionsManager.addTransaction(req.body);
+        console.log('[transactionsRouter.js] post res:', newTransaction);
         res.status(201).send(newTransaction);
     } catch (error) {
         res.status(500).send({ error })
